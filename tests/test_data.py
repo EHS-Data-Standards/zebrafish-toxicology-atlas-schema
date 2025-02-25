0b93100e05d198ee9269a1588b7e8e8a46048685
Sierra Taylor Moxon <sierra.taylor@gmail.com>
2025-02-24 17:46:07 -0800

    add doc templates config giles

--- tests/test_data.py
+++ tests/test_data.py
@@ -1,22 +1,24 @@
 """Data test."""
 import os
 import glob
-import unittest
-
-from linkml_runtime.loaders import yaml_loader
-from zebrafish_toxicology_atlas_schema.datamodel.zebrafish_toxicology_atlas_schema import GenotypeCollection
-
+from pathlib import Path
+import yaml
+from linkml.generators.pythongen import PythonGenerator
 ROOT = os.path.join(os.path.dirname(__file__), '..')
 DATA_DIR = os.path.join(ROOT, "src", "data", "examples")
 
 EXAMPLE_FILES = glob.glob(os.path.join(DATA_DIR, '*.yaml'))
+schemal_yaml =  file_path = Path(__file__).parent.parent / 'src' / 'zebrafish_toxicology_atlas_schema' / 'schema' / 'zebrafish_toxicology_atlas_schema.yaml'
+
+def test_make_python() -> str:
+    """
+    Generate python code from a schema
+
+    :return: python code as string
+    """
+    pstr = str(PythonGenerator(schemal_yaml, mergeimports=True).serialize())
+    return pstr
 
 
-class TestData(unittest.TestCase):
-    """Test data and datamodel."""
-
-    def test_data(self):
-        """Data test."""
-        for path in EXAMPLE_FILES:
-            obj = yaml_loader.load(path, target_class=GenotypeCollection)
-            assert obj
+def test_catalog_schema_validation():
+   pass
