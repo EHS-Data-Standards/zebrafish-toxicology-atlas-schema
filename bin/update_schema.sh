05d46c0c87e6f2357734756511bbd973d25ee52a
Sierra Taylor Moxon <sierra.taylor@gmail.com>
2025-10-31 17:19:49 -0700

    update schema according to discussions

---
+++ bin/update_schema.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+gen-linkml --materialize-patterns --materialize-attributes src/zebrafish_toxicology_atlas_schema/schema/zebrafish_toxicology_atlas_schema.yaml > schemas/zebrafish_toxicology_atlas_schema.json
